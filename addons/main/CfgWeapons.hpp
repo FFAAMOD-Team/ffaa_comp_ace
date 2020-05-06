b3ef4313324d6ccc5b464882e665825b597845d1
Mickyleitor <micky@uma.es>
2020-05-06 09:50:33 +0200

    mp5 inheritance and 9x19 added

--- addons/main/CfgWeapons.hpp
+++ addons/main/CfgWeapons.hpp
@@ -304,19 +304,19 @@ class CfgWeapons {
         };
     };
     // SUBFUSILES
-    class ffaa_armas_hkmp510a3: Rifle_Base_F {
-        ACE_barrelTwist=250;
-        ACE_barrelLength=146;
-    };
-    class ffaa_armas_hkmp5a5 : ffaa_armas_hkmp510a3 {
-        ACE_barrelTwist=250;
-        ACE_barrelLength=225;
-    };
-    class ffaa_armas_hkmp5pdw : ffaa_armas_hkmp510a3 {
+	class ffaa_armas_hkmp5pdw : Rifle_Base_F {
         ACE_barrelTwist=250;
         ACE_barrelLength=148;
     };
-    class ffaa_armas_ump: ffaa_armas_hkmp5a5 {
+    class ffaa_armas_hkmp510a3: ffaa_armas_hkmp5pdw {
+        ACE_barrelTwist=250;
+        ACE_barrelLength=146;
+    };
+    class ffaa_armas_hkmp5a5 : ffaa_armas_hkmp5pdw {
+        ACE_barrelTwist=250;
+        ACE_barrelLength=225;
+    };
+    class ffaa_armas_ump: ffaa_armas_hkmp5pdw {
         ACE_barrelTwist=305;
         ACE_barrelLength=200;
     };
