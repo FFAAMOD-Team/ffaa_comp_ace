19a83e1b1a781775a8b4358f5896c6733b0abc51
Mickyleitor <Mickyleitor@users.noreply.github.com>
2022-01-02 22:18:33 +0100

    Create CfgFactionClasses.hpp

---
+++ addons/main/CfgFactionClasses.hpp
@@ -0,0 +1,13 @@
+class CfgFactionClasses {
+    class FFAA {
+        ace_nametags_rankIcons[] = {
+            "\z\ace\addons\nametags\UI\icons_spain\private_gs.paa",
+            "\z\ace\addons\nametags\UI\icons_spain\corporal_gs.paa",
+            "\z\ace\addons\nametags\UI\icons_spain\sergeant_gs.paa",
+            "\z\ace\addons\nametags\UI\icons_spain\lieutenant_gs.paa",
+            "\z\ace\addons\nametags\UI\icons_spain\captain_gs.paa",
+            "\z\ace\addons\nametags\UI\icons_spain\major_gs.paa",
+            "\z\ace\addons\nametags\UI\icons_spain\colonel_gs.paa"
+        };
+    };
+};
